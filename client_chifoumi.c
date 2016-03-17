13dc217987e2ae64e50e700f6b34ca0116e3728c
leo <boisson.leo@gmail.com>
2016-03-18 00:48:41 +0100

    latex + chifoumi

---
+++ client_chifoumi.c
@@ -0,0 +1,114 @@
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#define SUCCESS 0
+#define ERROR 1
+#define SERVER_PORT 1500
+#define MAX_MSG 100
+
+int main(int argc, char * argv[])
+{
+
+    int sd, rc;
+    static char rcv_msg[MAX_MSG];
+
+    struct sockaddr_in localAddr, servAddr;
+    struct hostent * h;
+
+    struct timeval tv_envoi, tv_retour;
+    struct timezone tz;
+    long long diff;
+
+    //Verification du nombre d'arguments
+    if(argc != 2)
+    {
+        printf("Usage : %s <server>\n", argv[0]);
+        exit(ERROR);
+    }
+
+    //Recupere la structure de type hostent pour l'hote passe en param.
+    h = gethostbyname(argv[1]);
+    if(h == NULL)
+    {
+        printf("%s : Uncknow host '%s'", argv[0], argv[1]);
+        exit(ERROR);
+    }
+
+    //Initialisation des champs de la structure srvAddr
+    servAddr.sin_family = h->h_addrtype;
+    memcpy((char *)&servAddr.sin_addr.s_addr, h->h_addr_list[0], h->h_length);
+    servAddr.sin_port = htons(SERVER_PORT);
+
+    //Creation de la socket
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sd < 0)
+    {
+        perror("Cannot open socket : ");
+        exit(ERROR);
+    }
+
+    //Bind
+    localAddr.sin_family = AF_INET;
+    localAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    localAddr.sin_port = htons(0);
+
+    rc = bind(sd, (struct sockaddr *) &localAddr, sizeof(localAddr));
+    if(rc < 0)
+    {
+        printf("%s : cannot bind port TCP %u\n", argv[0], SERVER_PORT);
+        perror("Error : ");
+        exit(ERROR);
+    }
+
+    //Connexion au serveur
+    rc = connect(sd, (struct sockaddr *) &servAddr, sizeof(servAddr));
+    if(rc < 0)
+    {
+        perror("Cannot connect : ");
+        exit(ERROR);
+    }
+
+    //Recuperation du temps pre-envoi du message "echo"
+    gettimeofday(&tv_envoi, &tz);
+
+    //Envoi du message "echo"
+    rc = send(sd, "echo", strlen("echo")+1, 0); // +1 pour le \0
+    if(rc < 0)
+    {
+        printf("Cannot send data : ");
+        close(sd);
+        exit(ERROR);
+    }
+
+    printf("%s : data send (%s)\n", argv[0], "echo");
+
+    //Reception du message retourne par le serveur
+    rc = recv(sd,rcv_msg,MAX_MSG,0);
+    if(rc < 0)
+    {
+        printf("Cannot receive data : ");
+        close(sd);
+        exit(ERROR);
+    }
+
+    //Recuperation du temps post-reception du retour
+    gettimeofday(&tv_retour, &tz);
+
+    printf("%s : data received (%s)\n", argv[0], rcv_msg);
+
+     //Calcul de la difference entre les deux temps
+    diff=(tv_retour.tv_sec-tv_envoi.tv_sec) * 1000000L + (tv_retour.tv_usec-tv_envoi.tv_usec);
+
+    printf("Durée A/R : %llu usec\n",diff);
+
+    return(SUCCESS);
+}
---
+++ server_chifoumi.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+
+#define SUCCESS 0
+#define ERROR 1
+#define SERVER_PORT 1500
+#define MAX_MSG 100
+
+#define END_LINE 0x0
+
+
+
+//Declaration de la fonction read_line
+int read_line (int newSd,char * line_to_return);
+
+int main(int argc,char *argv[])
+{
+
+    int sd,newSd, r;
+    socklen_t cliLen;
+
+    struct sockaddr_in cliAddr, servAddr;
+    char line[MAX_MSG];
+
+    //Creation de la socket
+    sd = socket(AF_INET,SOCK_STREAM,0);
+    if(sd<0)
+    {
+        perror("Cannot open socket");
+        exit(ERROR);
+    }
+
+    //Initialisation des champs de la structure servAddr
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servAddr.sin_port = htons (SERVER_PORT);
+
+    //Bind
+    if(bind(sd,(struct sockaddr *) &servAddr, sizeof(servAddr))<0)
+    {
+        perror("Cannot bind port");
+        exit(ERROR);
+    }
+
+    //Listen
+    listen(sd,5);
+
+    while (1)
+    {
+        printf("%s : waiting for data on port TCP %u \n",argv[0],SERVER_PORT);
+        cliLen = sizeof(cliAddr);
+        newSd = accept(sd,(struct sockaddr *) &cliAddr,&cliLen);
+        if(newSd<0)
+        {
+            perror("Cannot accept connection");
+            exit(ERROR);
+        }
+
+        memset(line,0x0,MAX_MSG);
+
+        /* Reception de la requete */
+        while(read_line(newSd,line)!= ERROR)
+        {
+            printf("%s: received from %s:TCP%d : %s \n",argv[0],inet_ntoa(cliAddr.sin_addr),ntohs(cliAddr.sin_port),line);
+
+            //Envoi de la reponse
+            r = send(newSd, line, strlen(line)+1, 0); // +1 pour le \0
+            if(r < 0)
+            {
+                printf("Cannot send data : ");
+                close(newSd);
+                exit(ERROR);
+            }
+            memset(line,0x0,MAX_MSG);
+        }
+
+    }
+
+    return(SUCCESS);
+}
+
+int read_line (int newSd,char * line_to_return)
+{
+    static int rcv_ptr=0;
+    static char rcv_msg[MAX_MSG];
+    static int n;
+    int offset;
+
+    offset=0;
+
+    while(1)
+    {
+        if(rcv_ptr==0)
+        {
+            /* read data from socket */
+            memset(rcv_msg,0x0,MAX_MSG);/* init buffer */
+            n = recv(newSd,rcv_msg,MAX_MSG,0); /* wait for data */
+            if(n < 0)
+            {
+                perror("Cannot receive data ");
+                return ERROR;
+            }
+            else if(n==0)
+            {
+                printf("Connection closed by client \n");
+                close(newSd);
+                return ERROR;
+            }
+        }
+
+        /* if new data read on socket */
+        /* OR */
+        /* if another lise is still in buffer */
+        /* copy ligne into 'line_to_return'*/
+        while(*(rcv_msg+rcv_ptr)!=END_LINE && rcv_ptr<n)
+        {
+            memcpy(line_to_return+offset,rcv_msg+rcv_ptr,1);
+            offset++;
+            rcv_ptr++;
+        }
+
+        /* end of line + end of buffer => return line */
+        if(rcv_ptr == n-1)
+        {
+            /* set last by to END_LINE */
+            *(line_to_return+offset)=END_LINE;
+            rcv_ptr=0;
+            return ++offset;
+        }
+
+
+        /* end of line but still some data in buffer => return line */
+        if(rcv_ptr < n-1)
+        {
+            /* set last by to END_LINE */
+            *(line_to_return+offset)=END_LINE;
+            rcv_ptr++;
+            return ++offset;
+        }
+
+        /* end of buffer but line is not ended => */
+        /* wait for more data to arrive on socket */
+        if(rcv_ptr == n)
+        {
+            rcv_ptr = 0;
+        }
+    }
+}
