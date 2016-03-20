6409ddf1d5ebce63381d1d538ef806fa0829961a
leo <boisson.leo@gmail.com>
2016-03-20 16:35:02 +0100

    UDP

---
+++ UDP/appliclient.c
@@ -0,0 +1,60 @@
+/* Exemple de client UDP */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUCCESS 0
+#define ERROR 1
+#define MAX_MSG 100
+
+int main(int argc, char* argv[])
+{
+
+    int sockfd;
+    int port;
+    int i,n;
+    struct sockaddr_in servaddr;
+    char recvline[MAX_MSG];
+
+    //Verification du nombre d'arguments
+    if(argc < 4)
+    {
+        printf("Usage : %s <server> <port> <data1> <data2> ... <dataN>\n", argv[0]);
+        exit(ERROR);
+    }
+
+    //Creation de la socket
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    if(sockfd < 0)
+    {
+        perror("Cannot open socket : ");
+        exit(ERROR);
+    }
+
+    //Initialisation des champs de la structure servaddr
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+    port = atoi(argv[2]);
+    servaddr.sin_port=htons(port);
+
+    for(i=3; i < argc; i++)
+    {
+        //Envoi du mot
+        sendto(sockfd,argv[i],strlen(argv[i]),0, (struct sockaddr *)&servaddr,sizeof(servaddr));
+
+        //Recupetation du retour renvoye par le serveur
+        n=recvfrom(sockfd,recvline,MAX_MSG,0,NULL,NULL);
+        recvline[n]='\0';
+
+        //Affichage du retour renvoye par le serveur
+        printf("Retour --> %s \n", recvline);
+    }
+
+    exit(SUCCESS);
+
+}
---
+++ UDP/appliserveur.c
@@ -0,0 +1,65 @@
+/* Exemple de serveur UDP */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SUCCESS 0
+#define ERROR 1
+#define MAX_MSG 100
+
+int main(int argc, char** argv)
+{
+
+    int sockfd,n;
+    int port;
+    struct sockaddr_in servaddr,cliaddr;
+    socklen_t len;
+    char mesg[MAX_MSG];
+
+    //Verification du nombre d'arguments
+    if(argc != 2)
+    {
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(ERROR);
+    }
+
+    //Creaction de la socket
+    sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    if(sockfd<0)
+    {
+        perror("Cannot open socket ");
+        exit(ERROR);
+    }
+
+    //Initialisation des champs de la structure servaddr
+    bzero(&servaddr,sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+    port = atoi(argv[1]);
+    servaddr.sin_port=htons(port);
+
+    //Bind
+    if(bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
+    {
+        perror("Can't bind local adresse ");
+        return(ERROR);
+    }
+
+    for (;;)
+    {
+        len = sizeof(cliaddr);
+	//Reception des mots
+        n = recvfrom(sockfd,mesg,MAX_MSG,0,(struct sockaddr *)&cliaddr,&len);
+        mesg[n] = 0;
+        printf("Recu :\n");
+        printf("%s\n",mesg);
+        //Envoi du retour
+        sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+    }
+
+    exit(SUCCESS); //code mort
+}
+
